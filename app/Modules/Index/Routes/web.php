e22fd37a8516ae9c4d5b2a8932614f81d667f468
KristianZlatkov <44204924+kristianzlatkov@users.noreply.github.com>
2021-03-24 21:47:48 +0200

    Adding pages and index modules

--- app/Modules/Index/Routes/web.php
+++ app/Modules/Index/Routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +11,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'Modules\Index\Http\Controllers\IndexController@index');
-
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
+Route::prefix('index')->group(function() {
+    Route::get('/', 'IndexController@index');
 });
