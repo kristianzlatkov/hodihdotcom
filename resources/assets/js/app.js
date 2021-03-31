a1515206093e29a52c50a2b14a3aa11a547bc064
g.ginev <gosho.ginev@gmail.com>
2021-03-31 22:06:21 +0300

    added jquery and bootstrap

---
+++ resources/assets/js/app.js
@@ -0,0 +1,9 @@
+window.Popper = require('popper.js').default;
+require('popper.js');
+require('es6-promise').polyfill();
+require('jquery');
+var $ = require('jquery');
+import jquery from 'jquery';
+global.jQuery = jquery;
+global.$ = jquery;
+global.jquery = jquery;
