6394948c8963bbd36ed0df571704441dd532bc62
g.ginev <gosho.ginev@gmail.com>
2021-04-07 22:33:09 +0300

    added logo

--- app/Modules/Index/Resources/views/index.blade.php
+++ app/Modules/Index/Resources/views/index.blade.php
@@ -4,5 +4,10 @@
     <div class="index-view">
         <div class="welcome-bg-logo animate__animated animate__fadeIn"></div>
         <div class="welcome-bg" style="background-image: url('{{asset('assets/images/bg.png')}}')"></div>
+        <div class="logo animate__animated animate__fadeIn">
+            <a href="/" title="@lang('front.site_title')">
+                <img class="img-fluid" src="{{asset('assets/images/logo.svg')}}" alt="@lang('front.site_title')"/>
+            </a>
+        </div>
     </div>
 @endsection
