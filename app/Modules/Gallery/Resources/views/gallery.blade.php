10da9efd487feac4d89584e3ca9bb360d28ab759
g.ginev <gosho.ginev@gmail.com>
2021-05-05 13:34:52 +0300

    added pagination

--- app/Modules/Gallery/Resources/views/gallery.blade.php
+++ app/Modules/Gallery/Resources/views/gallery.blade.php
@@ -4,10 +4,11 @@
     <div class="container-sm">
         <div id="lightgallery" class="row">
             @foreach($images as $image)
-                <div class="col-12 col-md-6 col-lg-4" data-src="{{asset(\Illuminate\Support\Facades\Storage::url($image))}}">
+                <div class="col-12 col-md-6 col-lg-4 my-2" data-src="{{asset(\Illuminate\Support\Facades\Storage::url($image))}}">
                     @include('components.photo-list', ['url' => $image])
                 </div>
             @endforeach
         </div>
+        {{$images->links('components.pagination')}}
     </div>
 @endsection
